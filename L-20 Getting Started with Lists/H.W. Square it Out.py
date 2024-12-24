e375965910f4226c2821d5053bee9083f1046094
Vishodhan-Codez <vishodhan.ks@gmail.com>
2024-12-24 17:40:46 +0530

    list hw

---
+++ L-20 Getting Started with Lists/H.W. Square it Out.py
@@ -0,0 +1,9 @@
+def square_and_separate(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    os = [x for x in squares if x % 2 != 0]
+    es = [x for x in squares if x % 2 == 0]
+    print("Odd Squares:", os)
+    print("Even Squares:", es)
+sr = int(input("Enter the start of the range: "))
+er = int(input("Enter the end of the range: "))
+square_and_separate(sr,er)
